7f110e8e4c4dfe2d0aabc041629d3fe78cce11e2
Hamid <khmahdee@gmail.com>
2022-07-01 04:34:56 +0330

    add tokens and patterns

--- Phase 1/lexer.py
+++ Phase 1/lexer.py
@@ -1,6 +1,8 @@
 from sly import Lexer
 
-class JLexer(Lexer): 
+
+class JLexer(Lexer):
+
 
     tokens = {
         LBRACE, RBRACE, COL, SEMI,
@@ -12,9 +14,35 @@ class JLexer(Lexer):
         TYPE, ID, EQUAL, LEQUAL, GEQUAL, NEQUAL,TRUE,FALSE,INT,DOUBLE
     }
 
+    ignore = '[ \t]'
+    ignore_comments = r'\/\/.*'
+    ignore_multiline_comments = r'\/\*[^*]*\*+(?:[^/*][^*]*\*+)*\/'
 
-
-
+    LPAREN = r'\('
+    RPAREN = r'\)'
+    LBRACE = r'{'
+    RBRACE = r'}'
+    SEMI = r';'
+    COL = r':'
+    PLUSPLUS = r'\+\+'
+    MINUSMINUS = r'\-\-'
+    PLUS = r'\+'
+    MINUS = r'\-'
+    DIVIDE = r'\/'
+    TIMES = r'\*'
+    MOD = r'\%'
+    EQUAL = r'\=\='
+    NEQUAL = r'\!\='
+    GEQUAL = r'>='
+    LEQUAL = r'<='
+    GREATERT = r'>'
+    LESST = r'<'
+    ASSIGN = r'\='
+    AND = r'\&\&'
+    OR = r'\|\|'
+    NOT = r'\!(?!\W)'
+    CHAR_LIT = r'\'(.?|\\.)\''
+    STRING_LIT = r'(\".*\")'
 
     @_(r'-?\d+\.\d+')
     def FLOAT(self, t):
@@ -26,9 +54,35 @@ class JLexer(Lexer):
         t.value = int(t.value)
         return t
 
+    ID = r'[a-zA-Z_]+[a-zA-Z0-9_]*'
+
+    ID['int'] = INT
+    ID['long'] = TYPE
+    ID['float'] = FLOATNUM
+    ID['double'] = DOUBLE
+    ID['char'] = CHAR
+    ID['String'] = STRING
+    ID['boolean'] = BOOLEAN
+    ID['true'] = TRUE
+    ID['false'] = FALSE
+    ID['for'] = FOR
+    ID['while'] = WHILE
+    ID['break'] = BREAK
+    ID['continue'] = CONTINUE
+    ID['if'] = IF
+    ID['else'] = ELSE
+    ID['switch'] = SWITCH
+    ID['case'] = CASE
+    ID['default'] = DEFAULT
+    ID['void'] = VOID
+    ID['main'] = MAIN
+    ID['return'] = RETURN
+
+
     @_(r'\n+')
-    def ignore_enter(self, t): 
+    def ignore_enter(self, t):
         self.lineno += t.value.count('\n')
+
 
     def find_column(self, text, token):
         last_cr = text.rfind('\n', 0, token.index)
@@ -36,6 +90,7 @@ class JLexer(Lexer):
             last_cr = 0
         column = (token.index - last_cr) + 1
         return column
+
 
     def error(self, t):
         print(f'ERROR! Line {self.lineno}: Bad Character {t.value[0]}')
